6f1da58f591eed1344ad2b4f6b9fc17518530f3f
Victor Rattis <victorrattis@gmail.com>
2021-01-23 21:21:49 -0300

    Include a built apk

--- hello-gradle-app/Android.mk
+++ hello-gradle-app/Android.mk
@@ -4,26 +4,22 @@ include $(CLEAR_VARS)
 
 # O nome do module, com este nome você pode adicionar em algum makefile para 
 # adiciona-lo em uma arvore de compilação de algum produto.
-LOCAL_PACKAGE_NAME := HelloApp
+LOCAL_MODULE := HelloApp
+
+# Referencia os arquivo de codigo do modulo, nessa caso o apk.
+LOCAL_SRC_FILES := app-debug.apk
 
 LOCAL_MODULE_TAGS := optional
-
-# Referencia os arquivo de codigo do projeto, nesse caso, ele vai pegar todos
-# os arquivos java dentro de src/.
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-# A versão do Android SDK que irá ser usada para compilar o projeto.
-LOCAL_SDK_VERSION := current
 
 # Especifica o certificado que irá assinar o APK depois de compilado. Alguns
 # valores:
 # 	- platform, o Apk irá ser assinado com a mesma assinatura que a plataforma
 #	(Android SO);
 # 	- PRESIGNED, Não irá assinar o APK por que ele já está assinado.
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 
 # Quando true, o app vai ter privilegio de sistema e ele vai ser instalado em
 # priv-app/. E false, um app normal e é instaldo em apps/
 LOCAL_PRIVILEGED_MODULE := true
 
-include $(BUILD_PACKAGE)
+include $(BUILD_PREBUILT)
